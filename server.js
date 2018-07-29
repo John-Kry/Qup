81e072bd8333f494c80ce832a3e0d8d8f036825c
John Kryspin <johnkryspin@gmail.com>
2018-07-28 20:00:55 -0400

    make it better

---
+++ server.js
@@ -0,0 +1,12 @@
+const WebSocket = require('ws');
+ 
+const port = proccess.env.port || 3000;
+const wss = new WebSocket.Server({ port: port });
+ 
+wss.on('connection', function connection(ws) {
+  ws.on('message', function incoming(message) {
+    console.log('received: %s', message);
+  });
+ 
+  ws.send('something');
+});
