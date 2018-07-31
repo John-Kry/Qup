64161ee3762344b956d3ceb96de7f843e2eb53c7
John Kryspin <johnkryspin@gmail.com>
2018-07-31 18:15:49 -0400

    sockito

--- server.js
+++ server.js
@@ -1,12 +1,18 @@
-const WebSocket = require('ws');
- 
-const port = process.env.port || 3000;
-const wss = new WebSocket.Server({ port: port });
- 
-wss.on('connection', function connection(ws) {
-  ws.on('message', function incoming(message) {
-    console.log('received: %s', message);
+var app = require('express')();
+var http = require('http').Server(app);
+var io = require('socket.io')(http);
+
+app.get('/', function(req, res){
+  res.sendFile(__dirname + '/index.html');
+});
+
+io.on('connection', function(socket){
+    console.log("connected")
+    socket.on('chat message', function(msg){
+      console.log('message: ' + msg);
+    });
   });
- 
-  ws.send('something');
+
+http.listen(3000, function(){
+  console.log('listening on *:3000');
 });
