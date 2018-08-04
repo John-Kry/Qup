42cfac8512314f7006cc7dc04ac0e4c899542340
John Kryspin <johnkryspin@gmail.com>
2018-08-04 13:24:25 -0400

    initial commit

--- server.js
+++ server.js
@@ -21,6 +21,6 @@ io.on('connection', function(socket){
   });
 let port = process.env.PORT ||3000;
 http.listen(port, function(){
-  console.log('listening on *:3000');
+  console.log('listening on' + port);
 });
 console.log(port);
